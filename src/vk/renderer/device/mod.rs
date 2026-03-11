a08dbfa18fad90aff2765b05c4997a4845712082
gusahlg <gusahlg@gmail.com>
2026-03-11 20:12:57 +0100

    Added in additional device configuration steps

--- src/vk/renderer/device/mod.rs
+++ src/vk/renderer/device/mod.rs
@@ -1,9 +1,11 @@
 /// The file that exposes all of the device functionality, is supposed to act as a thin and
 /// practical api that keeps complicated details hidden.
+mod physical_device;
+use physical_device::*;
 
 struct Device {
     physical_device: ash::vk::PhysicalDevice,
-    logical_device: ash::vk::Device,
+    logical_device: ash::Device,
 
     graphics_queue: ash::vk::Queue,
     present_queue: ash::vk::Queue,
@@ -11,6 +13,25 @@ struct Device {
     command_pool: ash::vk::CommandPool,
 }
 impl Device {
-    pub fn get_graphics_queue() ->
+    pub fn configure_queues(&mut self, instance: &ash::Instance, surface_loader: &ash::khr::surface::Instance, surface: ash::vk::SurfaceKHR) {
+        // Pick physical device
+        let devices = instance.enumerate_physical_devices().expect("Could not find any physical devices!");
+
+        // Get a compatible device and save it and its graphics and present available queue family
+        // indices.
+        let queue_indices: (QueueFamiliesIndices, ash::vk::PhysicalDevice) = devices.into_iter()
+        .find_map(|device| {
+            acquire_queue_families(instance, device, surface_loader, surface).map(|v| {
+                (
+                    QueueFamiliesIndices {
+                        graphics: v.graphics,
+                        present: v.present,
+                    },
+                    device,
+                )
+            })
+        })
+        .expect("No suitable physical device found");
+    }
 }
 
--- src/vk/renderer/device/physical_device.rs
+++ src/vk/renderer/device/physical_device.rs
@@ -1,45 +1,47 @@
 /// This file is for setting up the physical device and should make all the checks associated with
 /// doing so as well as holding additional metadata.
-pub fn get_queues(instance: &ash::Instance, physical_device: ash::vk::PhysicalDevice) -> Vec<ash::vk::QueueFamilyProperties> {
+fn get_queues(instance: &ash::Instance, physical_device: ash::vk::PhysicalDevice) -> Vec<ash::vk::QueueFamilyProperties> {
     unsafe { instance.get_physical_device_queue_family_properties(physical_device) }
 }
 
-// Move to mod.rs
-pub fn configure_queue_families(instance: &ash::Instance, physical_device: ash::vk::PhysicalDevice, surface_loader: &ash::khr::surface::Instance, surface: ash::vk::SurfaceKHR) -> QueueFamiliesIndices {
+pub fn acquire_queue_families(instance: &ash::Instance, physical_device: ash::vk::PhysicalDevice, surface_loader: &ash::khr::surface::Instance, surface: ash::vk::SurfaceKHR) -> Option<QueueFamiliesIndices> {
     let queues = get_queues(instance, physical_device);
 
-    // Store indices
-    let graphics_queue_idx: u32;
-    let present_queue_idx: u32;
+    let mut graphics_queue_idx: Option<u32> = None;
+    let mut present_queue_idx: Option<u32> = None;
 
-    let supports_graphics: bool;
-    let supports_present: bool;
     for (index, family) in queues.iter().enumerate() {
         let index = index as u32;
 
-        supports_graphics = family.queue_flags.contains(ash::vk::QueueFlags::GRAPHICS);
+        let supports_graphics = family.queue_flags.contains(ash::vk::QueueFlags::GRAPHICS);
 
-        supports_present = unsafe {
+        let supports_present = unsafe {
             surface_loader.get_physical_device_surface_support(
                 physical_device,
                 index,
                 surface,
-            )?
+            ).unwrap_or(false)
         };
-        
-        if supports_graphics {
-            graphics_queue_idx = index;
+
+        if supports_graphics && graphics_queue_idx.is_none() {
+            graphics_queue_idx = Some(index);
         }
-        else if supports_present {
-            present_queue_idx = index;
+        if supports_present && present_queue_idx.is_none() {
+            present_queue_idx = Some(index);
         }
-        else if supports_graphics && supports_present {
-            return QueueFamiliesIndices { graphics: graphics_queue_idx, present: present_queue_idx };
+
+        if graphics_queue_idx.is_some() && present_queue_idx.is_some() {
+            break;
         }
     }
+
+    Some(QueueFamiliesIndices {
+        graphics: graphics_queue_idx?,
+        present: present_queue_idx?,
+    })
 }
 
 pub struct QueueFamiliesIndices {
-    graphics: u32,
-    present: u32,
+    pub graphics: u32,
+    pub present: u32,
 }
